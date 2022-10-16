c332e41215ac766de93e439931debcaeae8fd7d4
Baran12344 <69864153+Baran12344@users.noreply.github.com>
2022-10-16 11:09:08 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -75,7 +75,7 @@ Vote DDoS music</a>
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
                   <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
+                  <p className="text-3xl text-white font-semibold">EN İYİ MUZİK BOTU</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel bot olan DDoS music ile en iyi partileri vermeye hazır mısınız?</p>
                 </div>
                 <img src="https://cdn.discordapp.com/attachments/904763470623428738/1031115549855457290/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
