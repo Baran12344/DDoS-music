2f6a3e43f2605d62e6ca80ad69a3e3e46a968c70
Baran12344 <69864153+Baran12344@users.noreply.github.com>
2022-10-16 15:26:04 +0300

    Update Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -16,8 +16,20 @@ export default function Footer() {
                             </div>
                             <Menu as="div" className="relative mt-3 inline-block text-left">
                                 <div>
-                            </Menu>
-        <div className="py-1"></div>
+                                    <Menu.Button className="flex z-1 items-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                                        <img
+                                            src={`https://flagcdn.com/w80/gb.png`}
+                                            width="24"
+                                            height="18"
+                                            className="mr-2 h-4 rounded-sm"
+                                        />
+                                        English
+                                        <ChevronDownIcon
+                                            className="w-5 h-5 ml-2 -mr-1 transform rotate-180 text-violet-200 hover:text-violet-100"
+                                            aria-hidden="true"
+                                        />
+                                    </Menu.Button>
+<div className="py-1"></div>
 <a href="https://top.gg/bot/924325576095973426">
   <img src={"https://top.gg/api/widget/servers/924325576095973426.svg"}/>
 </a>
@@ -29,6 +41,9 @@ export default function Footer() {
 <a href="https://top.gg/bot/924325576095973426">
   <img src={"https://top.gg/api/widget/owner/924325576095973426.svg"}/>
 </a>
+                                </div>
+                            </Menu>
+
                                 </div>
                         </div>
                         <div className="col-span-1">
